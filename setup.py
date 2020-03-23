d531e9d5606293be3c1800acfdad1f98e7e4577c
Eduardo Pereira <pereira.somoza@gmail.com>
2020-03-23 13:52:31 -0300

    Correção no setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     packages=find_packages("src"),
     package_dir={"":"src"},
     description="Bound Box 2D",
-    lond_description=read("README.md"),
+    long_description=read("README.md"),
     long_description_content_type="text/markdown",
     url="https://github.com/duducosmos/boundbox2d",
     install_requires=["numpy"]
